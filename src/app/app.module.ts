27060cdeac17f6faa58d9dc455245beb80d43ff9
Angular-istic <60311082+Angular-istic@users.noreply.github.com>
2022-02-05 10:37:15 +0530

    gallery-lightbox-initial-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } 
+  from '@angular/platform-browser/animations';
+import { GalleryLightboxComponent } from './gallery-lightbox/gallery-lightbox.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GalleryLightboxComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/gallery-lightbox/gallery-lightbox.component.ts
@@ -0,0 +1,87 @@
+import { 
+  animate, 
+  style, 
+  transition,
+  trigger,
+  AnimationEvent
+} from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
+
+interface Item {
+  imageSrc: string;
+  imageAlt: string;
+}
+
+@Component({
+  selector: 'app-gallery-lightbox',
+  templateUrl: './gallery-lightbox.component.html',
+  styleUrls: ['./gallery-lightbox.component.scss'],
+  animations: [
+    trigger('animation', [
+      transition('void => visible', [
+        style({transform: 'scale(0.5)'}),
+        animate('150ms', style({transform: 'scale(1)'}))
+      ]),
+      transition('visible => void', [
+        style({transform: 'scale(1)'}),
+        animate('150ms', style({transform: 'scale(0.5)'}))
+      ]),
+    ]),
+    trigger('animation2', [
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('50ms', style({opacity: 0.8}))
+      ])
+    ])
+  ]
+})
+export class GalleryLightboxComponent implements OnInit {
+  @Input() galleryData: Item[] = [];
+  @Input() showCount = false;
+
+  previewImage = false;
+  showMask = false;
+  currentLightboxImage: Item = this.galleryData[0];
+  currentIndex = 0;
+  controls = true;
+  totalImageCount = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.totalImageCount = this.galleryData.length;
+  }
+
+  onPreviewImage(index: number): void {
+    this.showMask = true;
+    this.previewImage = true;
+    this.currentIndex = index;
+    this.currentLightboxImage = this.galleryData[index];
+  }
+
+  onAnimationEnd(event: AnimationEvent) {
+    if(event.toState === 'void') {
+      this.showMask = false;
+    }
+  }
+
+  onClosePreview() {
+    this.previewImage = false;
+  }
+
+  next(): void {
+    this.currentIndex = this.currentIndex + 1;
+    if(this.currentIndex > this.galleryData.length - 1) {
+      this.currentIndex = 0;
+    }
+    this.currentLightboxImage = this.galleryData[this.currentIndex];
+  }
+
+  prev(): void {
+    this.currentIndex = this.currentIndex - 1;
+    if(this.currentIndex < 0) {
+      this.currentIndex = this.galleryData.length - 1;
+    }
+    this.currentLightboxImage = this.galleryData[this.currentIndex];
+  }
+}
